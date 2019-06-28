6f9b06ed43763065bd35eabe7c3ce8a888ff7a89
Mike Huitrado <huitrado.mike@gmail.com>
2019-06-27 23:14:54 -0300

    STYLE: fix style and add new scripts

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -16,9 +16,9 @@ export default async (req, res, next) => {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
     req.userId = decoded.id;
+
+    return next();
   } catch (err) {
     return res.status(401).json({ error: 'Invalid token' });
   }
-
-  next();
 };
